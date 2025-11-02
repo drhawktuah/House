df4d1c2cabc0241b69abb587a0558409d79010ab
drhawktuah <jrbgnuiojnebwrugnbwr@proton.me>
2025-11-02 14:29:24 +0000

    typo

--- House.Services/Database/StarboardEntry.cs
+++ House.Services/Database/StarboardEntry.cs
@@ -1,8 +1,15 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace House.House.Services.Database;
 
 public class StarboardEntry : DatabaseEntity
 {
+    [BsonElement("message_id")]
     public ulong MessageID { get; set; } = default!;
-    public ulong StaboardMessageID { get; set; } = default!;
+
+    [BsonElement("starboard_message_id")]
+    public ulong StarboardMessageID { get; set; } = default!;
+
+    [BsonElement("guild_id")]
     public ulong GuildID { get; set; } = default!;
 }
